232f80fb4b992b2ce6c2e83d18c572eb726f3d80
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-01-22 20:35:36 +0000

    Update 1/22

---
+++ Prog54b.py
@@ -0,0 +1,8 @@
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+num3 = int(input("Number 3: "))
+num4 = int(input("Number 4: "))
+numsum = num1+num2+num3+num4
+avg = numsum / 4
+print("Sum of your numbers:", numsum)
+print("Average of your numbers:", avg)
---
+++ Prog54c.py
@@ -0,0 +1,7 @@
+import math
+pi = math.pi
+rad = int(input("Radius: "))
+circum = 2 * pi * rad
+area = pi * rad**2
+print("Circumference:", circum)
+print("Area:", area)
