08fcf92ee57ec3ab067da85961a0631229f3d492
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-03-14 19:36:01 +0000

    Update 3/14 (did not work, fix Cl213f.py)

--- Cl213f.py
+++ Cl213f.py
@@ -5,8 +5,14 @@ class Cl213f:
         self.cost = 0.0
 
     def calc(self):
-        # TODO
-        pass
+      for i in range(self.kwh):
+        if i <= 2000:
+          self.cost += 0.07
+        if i > 2000 and i <= 10_000:
+          self.cost += 0.05
+        if i > 10_000:
+          self.cost += 4
+      return self.cost
 
     def __str__(self):
         return f"The cost of {self.kwh} is {self.calc()}"
