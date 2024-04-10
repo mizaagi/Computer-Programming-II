baecd2e4d30436aef8c6005041097030182713d3
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2024-04-10 14:37:50 -0500

    Update 4/10

---
+++ CSharp/CSharp/Prog54cConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog54cConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            double rad = double.Parse(Console.ReadLine());
+            double circum = 2 * 3.14 * rad;
+            double area = (rad * rad) * 3.14;
+            Console.WriteLine("Circumference: " + circum);
+            Console.WriteLine("Area: " + area);
+            Console.ReadKey();
+        }
+    }
+}
