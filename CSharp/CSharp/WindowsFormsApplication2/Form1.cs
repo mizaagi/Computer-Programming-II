c0101d3db4b896a6b07f4644d375ca697b597b13
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2024-04-18 14:47:07 -0500

    Update 4/18

--- CSharp/CSharp/WindowsFormsApplication2/Form1.cs
+++ CSharp/CSharp/WindowsFormsApplication2/Form1.cs
@@ -8,13 +8,48 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication2
+namespace Prog88a
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int sum = num1 + num2;
+            int diff = num1 - num2;
+            int prod = num1 * num2;
+            double avg = (double)sum / 2.0;
+            int abs = Math.Abs(diff);
+            // Math.Max and Math.Min
+            int max = 0;
+            int min = 0;
+
+            if (num1 > num2)
+            {
+                max = num1;
+            }
+            else
+            {
+                max = num2;
+            }
+
+            if (num1 <= num2)
+                min = num1;
+            else min = num2;
+            // else if
+
+            lblSum.Text = sum.ToString();
+            lblMax.Text = max.ToString();
+            lblMin.Text = min.ToString();
+            label12.Text = diff.ToString();
+            label11.Text = prod.ToString();
+
         }
     }
 }
