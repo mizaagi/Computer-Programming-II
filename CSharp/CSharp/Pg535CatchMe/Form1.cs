9f9f2a682d9b591a6d8722a9a11641e36205094e
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-05-03 14:36:45 -0500

    Update 5/3

--- CSharp/CSharp/Pg535CatchMe/Form1.cs
+++ CSharp/CSharp/Pg535CatchMe/Form1.cs
@@ -6,5 +6,16 @@ namespace Pg535CatchMe
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You got me!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        }
+
+        private string[] strCaption = { "Click here", "Try harder!",
+                                        "Try again", "Not even close",
+                                        "Where are you?", "I'm over here!",
+                                        "Slow, aren't you?"};
+        private Random rand = new Random();
     }
 }
