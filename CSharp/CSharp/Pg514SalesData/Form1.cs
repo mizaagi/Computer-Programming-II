958140b951ba04fda43c55b40201ffdc1b3ee8e4
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-05-14 14:36:42 -0500

    Update 5/14

---
+++ CSharp/CSharp/Pg514SalesData/Form1.cs
@@ -0,0 +1,10 @@
+namespace Pg514SalesData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CSharp/CSharp/StrInt16/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a string: ");
+string text = Console.ReadLine();
+Console.Write("Enter a substring to search for: ");
+string word = Console.ReadLine();
+bool result = searchText(text, word);
+Console.WriteLine($"Does \"{text}\" contain \"{word}\"?: {result}");
+Console.ReadLine();
+
+static bool searchText(string text, string search)
+{
+    int tLen = text.Length;
+    int sLen = search.Length;
+    if (sLen > tLen) return false;
+
+    for (int lcv = 0; lcv <= tLen - sLen; lcv++)
+        // if (text[lcv..(lcv+sLen)] == search)
+        if (text.Substring(lcv, sLen) == search)
+            return true; // Substring uses startIndex, length (not end)
+
+    return false;
+}
